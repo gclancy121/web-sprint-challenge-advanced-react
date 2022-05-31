6eb5d9e822636f12662cae85823a835e844cea84
Gage Clancy <gclancy121@gmail.com>
2022-05-31 16:19:37 -0500

    app is functional

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,35 +1,178 @@
-import React from 'react'
+import React, {useState} from 'react';
+import axios from 'axios';
 
 export default function AppFunctional(props) {
+  const initialState = {
+    coordinateX: 2,
+    coordinateY: 2,
+    message: '',
+    moves: 0,
+    grid: ['', '', '', '', 'B', '', '', '', ''],
+    value: ''
+  };
+  const [state, setState] = useState(initialState);
+  
+  const gridHandler = (coordinateX, coordinateY) => {
+    if (coordinateX === 1 && coordinateY === 1) {
+      return ['B', '', '', '', '', '', '', '', '']
+    }
+    if (coordinateX === 2 && coordinateY === 1) {
+      return ['', 'B', '', '', '', '', '', '', '']
+    }
+     if (coordinateX === 3 && coordinateY === 1) {
+      return ['', '', 'B', '', '', '', '', '', '']
+    }
+    if (coordinateX === 1 && coordinateY === 2) {
+      return ['', '', '', 'B', '', '', '', '', '']
+    }
+    if (coordinateX === 2 && coordinateY === 2) {
+      return ['', '', '', '', 'B', '', '', '', '']
+    }
+    if (coordinateX === 3 && coordinateY === 2) {
+      return ['', '', '', '', '', 'B', '', '', '']
+    }
+    if (coordinateX === 1 && coordinateY === 3) {
+      return ['', '', '', '', '', '', 'B', '', '']
+    }
+    if (coordinateX === 2 && coordinateY === 3) {
+      return ['', '', '', '', '', '', '', 'B', '']
+    }
+    if (coordinateX === 3 && coordinateY === 3) {
+      return ['', '', '', '', '', '', '', '', 'B']
+    }
+  }
+
+  const handleLeft = () => {
+    if(state.coordinateX === 1) {
+      setState({...state, 
+        coordinateX: 1,  
+        message: "you can't go left",
+        grid: gridHandler(state.coordinateX, state.coordinateY)});
+    }else{
+      setState({...state, 
+        coordinateX: state.coordinateX - 1, 
+        moves: state.moves +1,
+        message: '',
+        grid: gridHandler(state.coordinateX -1, state.coordinateY)
+      })
+    }
+  }
+  const handleRight = () => {
+    if(state.coordinateX === 3) {
+      setState({...state, 
+        coordinateX: 3,  
+        message: "you can't go right",
+        grid: gridHandler(state.coordinateX, state.coordinateY)
+      });
+    }else{
+      setState({...state, 
+        coordinateX: state.coordinateX + 1,  
+        moves: state.moves +1, 
+        message: '',
+        grid: gridHandler(state.coordinateX +1, state.coordinateY)
+      })
+    }
+  }
+  const handleUp = () => {
+    if (state.coordinateY === 1) {
+      setState({...state, 
+        coordinateY: 1, 
+        message: "you can't go up",
+        grid: gridHandler(state.coordinateX, state.coordinateY)
+      });
+    } else{
+    // when you press up, update the Y coordinate to subtract by 1
+    setState({...state, 
+      coordinateY: state.coordinateY - 1,  
+      moves: state.moves +1, 
+      message: '',
+      grid: gridHandler(state.coordinateX, state.coordinateY -1)
+      });
+    }
+    
+  }
+  const handleDown = () => {
+    //this checks if the max number has already been reached
+    if (state.coordinateY === 3) {
+      setState({...state, 
+        coordinateY: 3,  
+        message: "you can't go down",
+        grid: gridHandler(state.coordinateX, state.coordinateY)
+      });
+    } else{
+    // when you press down, update the Y coordinate to increase by 1
+    setState({...state, 
+      coordinateY: state.coordinateY + 1,  
+      moves: state.moves +1, 
+      message: '',
+      grid: gridHandler(state.coordinateX, state.coordinateY +1)
+    });
+  }
+}
+  const handleReset = () => {
+    setState({
+      coordinateX: 2,
+      coordinateY: 2,
+      message: '',
+      moves: 0,
+      grid: ['', '', '', '', 'B', '', '', '', ''],
+      value: ''
+    })
+  }
+
+  const handleChanges = (evt) => {
+    setState({...state, value: evt.target.value });
+  }
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const email = evt.target.email.value;
+    axios.post(`http://localhost:9000/api/result`, {x: state.coordinateX, y: state.coordinateY, steps: state.moves, email: email})
+    .then(res => {
+      console.log(res)
+      setState({...state, message: res.data.message})
+    })
+    .catch(err => {
+      console.log(err.message)
+     if (err.message === "Request failed with status code 403") {
+       setState({...state, message: 'foo@bar.baz failure #23'})
+     } else {
+       setState({...state, message: err.message})
+     }
+     })
+  }
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({state.coordinateX}, {state.coordinateY})</h3>
+        <h3 id="steps">You moved {state.moves} times</h3>
       </div>
       <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+      {state.grid.map((item, idx) => {
+            if (item === 'B') {
+              return (<div key = {idx} className = 'square active'>{item}</div>)
+            } else {
+            return (
+              <div key = {idx} className="square">{item}</div>
+            )}
+          })}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{state.message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={handleLeft}>LEFT</button>
+        <button id="up" onClick={handleUp}>UP</button>
+        <button id="right" onClick={handleRight}>RIGHT</button>
+        <button id="down" onClick={handleDown}>DOWN</button>
+        <button id="reset" onClick={handleReset}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={handleSubmit}>
+        <input id="email" 
+        type="email" 
+        placeholder="type email" 
+        onChange={handleChanges}
+        value={state.value}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
